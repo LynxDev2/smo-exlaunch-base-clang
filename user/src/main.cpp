66cd3b30ef544f944236ae9ae9586e22f95fb088
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2024-11-04 19:51:28 +0200

    Add ModDrawer example and osx support

--- user/src/main.cpp
+++ user/src/main.cpp
@@ -2,6 +2,8 @@
 #include <mallow/mallow.hpp>
 
 #include "ActorFactory/actorPatches.h"
+#include "ModDrawer/ModDrawer.h"
+#include "heap/seadHeapMgr.h"
 
 static void setupLogging() {
     using namespace mallow::log::sink;
@@ -34,6 +36,10 @@ using mallow::log::logLine;
 
 //Mod code
 
+static const char* DBG_FONT_PATH = "DebugData/Font/nvn_font_jis1.ntx";
+static const char* DBG_SHADER_PATH = "DebugData/Font/nvn_font_shader_jis1.bin";
+static const char* DBG_TBL_PATH = "DebugData/Font/nvn_font_jis1_tbl.bin";
+
 struct nnMainHook : public mallow::hook::Trampoline<nnMainHook>{
     static void Callback(){
         nn::fs::MountSdCardForDebug("sd");
@@ -45,8 +51,18 @@ struct nnMainHook : public mallow::hook::Trampoline<nnMainHook>{
     }
 };
 
+struct GameSystemInitHook : public mallow::hook::Trampoline<GameSystemInitHook>{
+    static void Callback(void* thisPtr){
+        ModDrawer::createInstance(sead::HeapMgr::instance()->getCurrentHeap());
+        ModDrawer::instance()->setup(DBG_SHADER_PATH, DBG_FONT_PATH, DBG_TBL_PATH);
+        ModDrawer::instance()->addMessage("Hello message", 6000, sead::Vector2f(40.f, 40.f));
+        Orig(thisPtr);
+    }
+};
+
 extern "C" void userMain() {
     exl::hook::Initialize();
     nnMainHook::InstallAtSymbol("nnMain");
+    GameSystemInitHook::InstallAtSymbol("_ZN10GameSystem4initEv");
     ca::actorPatches();
 }
