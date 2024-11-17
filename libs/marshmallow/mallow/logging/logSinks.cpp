a4d24dd031dad9ea12a2b9691e2c6ab0ffdac5b7
cookieso <cookiesogurke@gmail.com>
2024-11-17 17:26:47 +0100

    feat: add reconnect option for network logger

--- libs/marshmallow/mallow/logging/logSinks.cpp
+++ libs/marshmallow/mallow/logging/logSinks.cpp
@@ -1,3 +1,4 @@
+#include "logSinks.hpp"
 #include <alloca.h>
 #include <cstdio>
 #include <exl/lib.hpp>
@@ -86,64 +87,78 @@ namespace mallow::log::sink {
                         [](const char* row, std::size_t size) { svcOutputDebugString(row, size); });
     }
 
-    bool NetworkSink::initialized = false;
-
-    NetworkSink::NetworkSink(const char* host, u16 port) : mutex(false) {
-        if (!initialized) {
-            mallow::net::initializeNetwork();
-            
-            if (mallow::net::initializationResult().IsFailure()) {
-                dbg::debugPrint("NetworkSink: failed to initialize network");
-                return;
-            }
-
-            if ((fileDescriptor = nn::socket::Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-                dbg::debugPrint("NetworkSink: failed to create socket");
-                return;
-            }
-
-            struct in_addr hostAddress = {};
-            struct sockaddr_in serverAddress = {};
-            if (!nn::socket::InetAton(host, &hostAddress)) {
-                struct hostent* hostent = nn::socket::GetHostByName(host);
-                if (hostent && hostent->h_addr_list[0]) {
-                    hostAddress = *reinterpret_cast<struct in_addr*>(hostent->h_addr_list[0]);
-                } else {
-                    dbg::debugPrint("NetworkSink: failed to resolve host");
-                    return;
-                }
-            }
-            serverAddress.sin_addr = hostAddress;
-            serverAddress.sin_port = nn::socket::InetHtons(port);
-            serverAddress.sin_family = AF_INET;
-
-            dbg::debugPrint("NetworkSink: connecting to %s:%d", host, port);
-
-            s32 result = nn::socket::Connect(fileDescriptor,
-                                             reinterpret_cast<struct sockaddr*>(&serverAddress),
-                                             sizeof(serverAddress));
-
-            if (result < 0) {
-                dbg::debugPrint("NetworkSink: failed to connect to server");
-                return;
-            }
-
-            bool tcpNoDelayOpt = true;
-            nn::socket::SetSockOpt(fileDescriptor, 6, TCP_NODELAY, &tcpNoDelayOpt, sizeof(tcpNoDelayOpt));
-
-            dbg::debugPrint("NetworkSink: connected to server");
-
+    NetworkSink::NetworkSink(const char* host, u16 port, bool tryReconnect) : mutex(false), host(host), port(port) {
+        if (connect() != ConnectResult::SUCCESS && !tryReconnect)
             initialized = true;
-        }
     }
 
     void NetworkSink::send(const char* buffer, std::size_t size) {
-        if (fileDescriptor < 0)
-            return;
+        if (fileDescriptor < 0) {
+            if (initialized) {
+                return;
+            }
+            if (connect() != ConnectResult::SUCCESS)
+                return;
+        }
 
         mutex.Lock();
         nn::socket::Send(fileDescriptor, buffer, size, 0);
         mutex.Unlock();
+    }
+
+    NetworkSink::ConnectResult NetworkSink::connect() {
+        if (initialized) {
+            dbg::debugPrint("NetworkSink: Already initialized.");
+            return ConnectResult::ALREADY_INITIALIZED;
+        }
+        mallow::net::initializeNetwork();
+
+        if (mallow::net::initializationResult().IsFailure()) {
+            dbg::debugPrint("NetworkSink: failed to initialize network");
+            return ConnectResult::NETWORK_FAILED;
+        }
+
+        if ((fileDescriptor = nn::socket::Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+            dbg::debugPrint("NetworkSink: failed to create socket");
+            return ConnectResult::SOCKET_FAILED;
+        }
+
+        struct in_addr hostAddress = {};
+        struct sockaddr_in serverAddress = {};
+        if (!nn::socket::InetAton(host, &hostAddress)) {
+            struct hostent* hostent = nn::socket::GetHostByName(host);
+            if (hostent && hostent->h_addr_list[0]) {
+                hostAddress = *reinterpret_cast<struct in_addr*>(hostent->h_addr_list[0]);
+            } else {
+                dbg::debugPrint("NetworkSink: failed to resolve host");
+                fileDescriptor = -1;
+                return ConnectResult::RESOLVE_FAILED;
+            }
+        }
+        serverAddress.sin_addr = hostAddress;
+        serverAddress.sin_port = nn::socket::InetHtons(port);
+        serverAddress.sin_family = AF_INET;
+
+        dbg::debugPrint("NetworkSink: connecting to %s:%d", host, port);
+
+        s32 result =
+            nn::socket::Connect(fileDescriptor, reinterpret_cast<struct sockaddr*>(&serverAddress),
+                                sizeof(serverAddress));
+
+        if (result < 0) {
+            dbg::debugPrint("NetworkSink: failed to connect to server");
+            fileDescriptor = -1;
+            return ConnectResult::NO_SERVER;
+        }
+
+        bool tcpNoDelayOpt = true;
+        nn::socket::SetSockOpt(fileDescriptor, 6, TCP_NODELAY, &tcpNoDelayOpt,
+                               sizeof(tcpNoDelayOpt));
+
+        dbg::debugPrint("NetworkSink: connected to server");
+
+        initialized = true;
+        return ConnectResult::SUCCESS;
     }
 
     void NetworkSink::log(const char* fmt, va_list args) {
